d050683eb3079173da2469147361c71251e333ab
Silver3310 <epifanov998@mail.ru>
2019-12-05 15:04:18 +0700

    Init the repository

---
+++ app.py
@@ -0,0 +1,60 @@
+"""
+Example:
+    http://mylab.babah24.ru:5000/send/740f4707bebcf74f9b7c25d48e3358945f6aa01da5ddb387462c7eaf61bb78ad
+"""
+import socket
+import ssl
+import json
+import struct
+import binascii
+
+from flask import Flask
+from flask import Response
+
+app = Flask(__name__)
+
+
+@app.route("/send/<device_token>", methods=['GET'])
+def get_results(device_token):
+    """
+    Send a push notification to APN
+    (Apple Push Notification service)
+    """
+
+    pay_load = {}
+
+    cert = 'certificate.pem'
+
+    host = ('gateway.push.apple.com', 2195)
+
+    pay_load = json.dumps(pay_load, separators=(',', ':'))
+
+    device_token = binascii.unhexlify(device_token)
+    fmt = "!BH32sH{}s".format(len(pay_load))
+
+    msg = struct.pack(
+        fmt,
+        0,
+        32,
+        device_token,
+        len(pay_load),
+        bytes(pay_load, "utf-8")
+    )
+
+    ssl_sock = ssl.wrap_socket(
+        socket.socket(
+            socket.AF_INET,
+            socket.SOCK_STREAM
+        ),
+        certfile=cert,
+    )
+    ssl_sock.connect(host)
+
+    ssl_sock.write(msg)
+    ssl_sock.close()
+
+    return Response(status=200)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ background.sh
@@ -0,0 +1,3 @@
+chmod +x app.py
+nohup /home/alexey/pypass_apns/venv/bin/python3 /home/alexey/pypass_apns/app.py &
+ps aux | grep app.py
